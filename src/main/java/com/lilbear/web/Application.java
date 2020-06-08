5ff092ff0e8446996c819d9441aeb194d9096044
hwangbo <taekwonod714@naver.com>
2020-06-08 17:05:25 +0900

    feat: chapter 4 구현

--- src/main/java/com/lilbear/web/Application.java
+++ src/main/java/com/lilbear/web/Application.java
@@ -1,11 +1,44 @@
 package com.lilbear.web;
 
+import com.lilbear.web.domain.Board;
+import com.lilbear.web.domain.BoardType;
+import com.lilbear.web.domain.User;
+import com.lilbear.web.repository.BoardRepository;
+import com.lilbear.web.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) {
+        return (args) -> {
+            User user = userRepository.save(User.builder()
+                    .name("lilbear")
+                    .password("test")
+                    .email("lilbear@gamil.com")
+                    .createdDate(LocalDateTime.now())
+                    .build());
+
+            IntStream.rangeClosed(1, 200).forEach(index ->
+                    boardRepository.save(Board.builder()
+                            .title("게시글" + index)
+                            .subTitle("순서" + index)
+                            .content("콘텐츠")
+                            .boardType(BoardType.free)
+                            .createdDate(LocalDateTime.now())
+                            .updatedDate(LocalDateTime.now())
+                            .user(user)
+                            .build()));
+        };
     }
 }
